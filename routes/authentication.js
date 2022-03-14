204d7b7182979c77094c01d9d84ce5f452e079cd
jivko25 <jivkou@yahoo.com>
2022-03-14 15:52:57 +0200

    add validation with @hapi/joi

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/UserModel');
+const registerValidation = require('../validations/registerValidation');
 
 router.post('/login', (req, res) => {
     res.send('login')
 });
 
 router.post('/register', async (req, res) => {
+    const error = registerValidation(req.body);
+    if(error) return res.status(400).send(error);
+
     const user = new User({
         username : req.body.username,
         password : req.body.password,
