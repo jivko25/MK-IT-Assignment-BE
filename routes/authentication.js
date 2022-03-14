a5f8eb4a552856cca180d5936ad8b598dc2d80ec
jivko25 <jivkou@yahoo.com>
2022-03-15 00:16:56 +0200

    add additional validation when register
    1. Add jwt token
    2. Add checking if username exists

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,14 +2,24 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/UserModel');
 const registerValidation = require('../validations/registerValidation');
+const jwt = require('jsonwebtoken');
 
 router.post('/login', (req, res) => {
     res.send('login')
 });
 
 router.post('/register', async (req, res) => {
+    //Validate body
     const error = registerValidation(req.body);
     if(error) return res.status(400).send(error);
+
+    //Check if email exists
+    const checkIfEmailExist = await User.findOne({email : req.body.email});
+    if(checkIfEmailExist) return res.send(`User with email ${req.body.email} already exist`);
+
+    //Check if username exists
+    const checkIfUserNameExist = await User.findOne({username : req.body.username});
+    if(checkIfUserNameExist) return res.send(`User with username ${req.body.username} already exist`);
 
     const user = new User({
         username : req.body.username,
@@ -18,10 +28,15 @@ router.post('/register', async (req, res) => {
         movies : []
     })
     try {
-        const savedUser = await user.save();
-        res.send(savedUser);
+        await user.save();
+        const test = User.findOne({email : req.body.email})
+        const token = jwt.sign({_id : test._id}, process.env.secret);
+        res.send({
+            token,
+            username : req.body.username
+        });
     } catch (error) {
-        res.status(400).send('Error')
+        res.status(400).send(error)
     }
 });
 
