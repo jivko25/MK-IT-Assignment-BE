fd46886e6c60375be054d22bf762107d2d7cb79a
jivko25 <jivkou@yahoo.com>
2022-03-18 17:30:49 +0200

    install cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json({ extended: false }));
 
 const user = require('./routes/authentication');
